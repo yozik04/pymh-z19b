2a412222e164154c69d9270b0ebe55c681bec8a1
Jevgeni Kiski <yozik04@gmail.com>
2019-01-17 15:42:17 +0200

    Travis deploy to pypy.

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
-from setuptools import setup
+from os import path as p
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(filename, parent=None):
+    parent = (parent or __file__)
+
+    try:
+        with open(p.join(p.dirname(parent), filename)) as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+def parse_requirements(filename, parent=None):
+    parent = (parent or __file__)
+    filepath = p.join(p.dirname(parent), filename)
+    content = read(filename, parent)
+
+    for line_number, line in enumerate(content.splitlines(), 1):
+        candidate = line.strip()
+
+        if candidate.startswith('-r'):
+            for item in parse_requirements(candidate[2:].strip(), filepath):
+                yield item
+        else:
+            yield candidate
 
 setup(
   name='pymh-z19b-serial',
@@ -8,5 +38,19 @@ setup(
   license='MIT',
   author='Jevgeni Kiski',
   author_email='yozik04@gmail.com',
-  description='MH-Z19B CO2 sensor communication via serial port'
+  description='MH-Z19B CO2 sensor communication via serial port',
+  keywords = 'mh-z19b mh-z19 serial async',
+  classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3'
+  ],
+  install_requires=list(parse_requirements('requirements.txt')),
+  tests_require=[
+    'mock',
+    'pytest'
+  ]
 )
